e9d25fde93ffd75abbdda9f70cf09496e6fcf971
Anna1917 <105939470+Anna1917@users.noreply.github.com>
2026-06-22 12:57:35 +0500

    Update t2.js

--- 5/t2.js
+++ 5/t2.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
-const clients = JSON.parse(fs.readFileSync('./5/json/clients.json', 'utf8')).clients;
-
 console.log(JSON.stringify(
-  clients
+  JSON.parse(require('fs').readFileSync('./5/json/clients.json', 'utf8')).clients
     .filter(client => client.address.city === 'Кунгур')
     .sort((a, b) => 
       a.gender !== b.gender 
